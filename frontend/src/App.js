4f2df02cada1b7ffae5e0a5562f5b5fc99d1d7f8
Abdul Rauf Sultan <81006767+arsbringer65@users.noreply.github.com>
2022-12-20 01:41:32 +0800

    adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
