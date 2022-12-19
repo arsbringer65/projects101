e399cf73b760e5b209fb526bfb12c010612be0ad
Abdul Rauf Sultan <81006767+arsbringer65@users.noreply.github.com>
2022-12-19 23:14:42 +0800

    adding frontend

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div class="footer">
+            <h1>GC Cards</h1>
+        </div>
+    )
+}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>ID Card</h1>
+    </div>
+  )
+}
